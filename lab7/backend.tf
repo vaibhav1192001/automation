af300dd75b29190279df3440a4a82b79ba3f4caa
vaibhav1192001 <vaibhavkadia92854@gmail.com>
2024-11-13 17:12:15 -0500

    Delete lab7 directory

--- lab7/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-N01657870-RG"
-    storage_account_name = "tfstaten01657870sa"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
